baaf8dd4d01b358badefd259067a66fcba92a339
Roshit Massey <roshit.massey@kratikal.com>
2021-10-23 00:42:47 +0530

    add

---
+++ app/CompanyInformation.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CompanyInformation extends Model
+{
+    use SoftDeletes;
+    protected $table = 'company_informations';
+
+    protected $fillable = ['vendor_id', 'company_name','legal_company_name' ,'country_id', 'state_id', 'city', 'postal_code', 'registered_address', 'registered_address_2', 'contact_number', 'company_website','hear_about_us','terms_condition','bank_name', 'acc_no', 'acc_holder_name', 'ifsc'];
+
+    protected $appends = ['country', 'state'];
+
+    public function getCountryAttribute()
+    {
+    	$country = Country::where('id', $this->country_id)->first();
+    	if($country) {
+    		return $country->country_name;
+    	}
+    }
+
+    public function getStateAttribute()
+    {
+    	$state = State::where('id', $this->state_id)->first();
+    	if($state) {
+    		return $state->state_name;
+    	}
+    }
+}
